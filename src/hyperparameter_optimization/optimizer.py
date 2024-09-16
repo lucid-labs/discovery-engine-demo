00722ec4096416b90fb665155bb8b24dfb5e0e4d
Shivam Patel <patelshivam99@gmail.com>
2024-09-15 21:42:37 -0700

    Added Hyperparameter optimization

---
+++ src/hyperparameter_optimization/optimizer.py
@@ -0,0 +1,12 @@
+import optuna
+from typing import Callable
+
+class HyperparameterOptimizer:
+    def __init__(self, objective: Callable, n_trials: int = 100):
+        self.objective = objective
+        self.n_trials = n_trials
+
+    def optimize(self) -> dict:
+        study = optuna.create_study(direction='minimize')
+        study.optimize(self.objective, n_trials=self.n_trials)
+        return study.best_params
